9ea83599dc85ef15067acabe8725b57373a688b3
Anuja Bawaskar <anujabawaskar@gmail.com>
2018-06-20 18:32:45 -0400

    450. Delete Node in a BST

---
+++ BST/DeleteNodeinaBST.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null)
+            return null;
+        
+        if(key < root.val)
+            root.left = deleteNode(root.left, key);
+        else if(key > root.val)
+            root.right = deleteNode(root.right, key);
+        else {
+            if(root.left == null)
+                return root.right;
+            else if(root.right == null)
+                return root.left;
+            
+            TreeNode minNode = find(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+        }
+        return root;
+    }
+    public TreeNode find(TreeNode node) {
+        while(node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
