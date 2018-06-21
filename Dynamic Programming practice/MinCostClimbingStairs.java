a1c8117f09a62f72c1fca3b649c58b8d24db3460
Anuja Bawaskar <anujabawaskar@gmail.com>
2018-06-20 23:39:55 -0400

    Update MinCostClimbingStairs.java

--- Dynamic Programming practice/MinCostClimbingStairs.java
+++ Dynamic Programming practice/MinCostClimbingStairs.java
@@ -1,15 +1,26 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int len = cost.length;
-        int[] dp = new int[len];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        int answer = Math.min(helper(new int[len], 0, len, cost), helper(new int[len], 1, len, cost));
         
-        for(int i = 2; i < len; i++)
-            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
         
-        return Math.min(dp[len - 1], dp[len - 2]);
-        
+        return answer;
     }
     
+    public int helper(int[] dp, int low, int high, int[] cost) {
+        if(low == 0) {
+            dp[0] = cost[0];
+            dp[1] = Math.min(cost[1], dp[0] + cost[1]);
+        }
+        else {
+           dp[0] = Integer.MAX_VALUE; 
+           dp[1] = cost[1];
+        }
+        
+        for(int i = 2; i < high; i++)
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+         
+        
+        return Math.min(dp[high - 1], dp[high - 2]);
+    }
 }
